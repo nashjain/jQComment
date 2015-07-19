3b05079d5fc81435d1f75fb9e5e9760c44e7acd7
Naresh Jain <nashjain@gmail.com>
2015-07-19 16:55:55 +0530

    added PhantomJS added Package.json

--- karma.conf.js
+++ karma.conf.js
@@ -57,7 +57,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox', 'Chrome', 'Safari'],
+    browsers: ['PhantomJS', 'Firefox', 'Chrome', 'Safari'],
 
 
     // Continuous Integration mode
