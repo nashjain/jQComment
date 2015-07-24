a966d52d66d89f7a751d1a21f8e6cad93a5082f6
Naresh Jain <nashjain@gmail.com>
2015-07-24 05:30:20 +0530

    refactored tests to remove data duplication

--- spec/CommentsSpec.js
+++ spec/CommentsSpec.js
@@ -1,8 +1,10 @@
 describe("jQuery Comments Plugin", function () {
     var comments;
+    var singleCommentJSON;
     var getURL = 'http://localhost/jQComment/comments.php';
 
     beforeEach(function () {
+        singleCommentJSON = {comment: "First Comment", user: "Naresh", updatedOn: "2014-09-19T15:28:46.493Z"};
         setFixtures('<div id="comments"></div>');
         comments = $('#comments');
         jasmine.Ajax.install();
@@ -21,23 +23,19 @@ describe("jQuery Comments Plugin", function () {
     });
 
     it("Should add single comment as a div from passed in json", function () {
-        comments.jQComments({data: [{comment:"First Comment", user:"Naresh", updatedOn: "2014-12-19T15:28:46.493Z"}]});
+        comments.jQComments({data: [singleCommentJSON]});
         expect(comments).toContainElement('div.comment');
         expect(comments.find('div.comment').size()).toBe(1);
         expect(comments.find('div.comment div.comment-header')).toContainText('Naresh');
         expect(comments.find('div.comment div.comment-body')).toContainText('First Comment');
     });
 
-    it("Should add multiple comment in the same order", function () {
-        var opts = {data: [{comment: "First Comment", user: "Naresh", updatedOn: "2014-12-19T15:28:46.493Z"}, {comment: "Second Comment", user: "Jack", updatedOn: "2014-06-19T15:28:46.493Z"}]};
-        comments.jQComments(opts);
-        expect(comments.find('div.comment').size()).toBe(opts.data.length);
-        expect(comments.find('div.comment div.comment-header:first')).toContainText('Naresh');
-        expect(comments.find('div.comment div.comment-header:last')).toContainText('Jack');
-    });
-
     it("Should display the most recent comments on top", function () {
-        var opts = {data: [{comment: "First Comment", user: "Naresh", updatedOn: "2014-09-19T15:28:46.493Z"}, {comment: "Second Comment", user: "James", updatedOn: "2014-06-19T15:28:46.493Z"}, {comment: "Third Comment", user: "Jack", updatedOn: "2014-12-19T15:28:46.493Z"}]};
+        var opts = {
+            data: [singleCommentJSON,
+                {comment: "Second Comment", user: "James", updatedOn: "2014-06-19T15:28:46.493Z"},
+                {comment: "Third Comment", user: "Jack", updatedOn: "2014-12-19T15:28:46.493Z"}]
+        };
         comments.jQComments(opts);
         expect(comments.find('div.comment').size()).toBe(opts.data.length);
         expect(comments.find('div.comment div.comment-header:first')).toContainText('Jack');
@@ -45,43 +43,47 @@ describe("jQuery Comments Plugin", function () {
     });
 
     it("Should not display private comments to guest user", function () {
-        comments.jQComments({data: [{comment:"First Comment", user:"Naresh", private: true, updatedOn: "2014-12-19T15:28:46.493Z"}]});
+        singleCommentJSON.private = true;
+        comments.jQComments({data: [singleCommentJSON]});
         expect(comments.find('div.comment').size()).toBe(0);
         expect(comments).toContainText('Be the first one to comment...');
     });
 
     it("Should not display private comments to public", function () {
-        comments.jQComments({data: [{comment:"First Comment", user:"Naresh", private: true, updatedOn: "2014-12-19T15:28:46.493Z"}], loggedInUser:'Another User'});
+        singleCommentJSON.private = true;
+        comments.jQComments({data: [singleCommentJSON], loggedInUser: 'Another User'});
         expect(comments.find('div.comment').size()).toBe(0);
         expect(comments).toContainText('Be the first one to comment...');
     });
 
     it("Should display private comments to author", function () {
-        comments.jQComments({data: [{comment:"First Comment", user:"Naresh", private: true, updatedOn: "2014-12-19T15:28:46.493Z"}], loggedInUser: 'Naresh'});
+        singleCommentJSON.private = true;
+        comments.jQComments({data: [singleCommentJSON], loggedInUser: 'Naresh'});
         expect(comments.find('div.comment').size()).toBe(1);
         expect(comments.find('div.comment div.comment-header')).toContainText('Naresh');
     });
 
     it("Should fetch data from URL and display comments", function () {
-        var expectedAjaxResponse = [{comment: "First Comment", user: "Naresh", updatedOn: "2014-09-19T15:28:46.493Z"}, {comment: "Second Comment", user: "James", updatedOn: "2014-06-19T15:28:46.493Z"}, {comment: "Third Comment", user: "Jack", updatedOn: "2014-12-19T15:28:46.493Z"}];
-        var opts = {url: getURL};
-
+        var expectedAjaxResponse = [
+            singleCommentJSON,
+            {comment: "Second Comment", user: "James", updatedOn: "2014-06-19T15:28:46.493Z"},
+            {comment: "Third Comment", user: "Jack", updatedOn: "2014-12-19T15:28:46.493Z"}
+        ];
         jasmine.Ajax.stubRequest(getURL).andReturn({
-                status: 200,
-                statusText: 'HTTP/1.1 200 OK',
-                contentType: 'application/json;charset=UTF-8',
-                responseText: JSON.stringify(expectedAjaxResponse)
-            });
+            status: 200,
+            statusText: 'HTTP/1.1 200 OK',
+            contentType: 'application/json;charset=UTF-8',
+            responseText: JSON.stringify(expectedAjaxResponse)
+        });
 
-        comments.jQComments(opts);
+        comments.jQComments({url: getURL});
+
         expect(comments.find('div.comment').size()).toBe(expectedAjaxResponse.length);
         expect(comments.find('div.comment div.comment-header:first')).toContainText('Jack');
         expect(comments.find('div.comment div.comment-header:last')).toContainText('James');
     });
 
     it("Should try to fetch data from URL and display error message when unsuccessful", function () {
-        var opts = {url: getURL};
-
         jasmine.Ajax.stubRequest(getURL).andReturn({
             status: 400,
             statusText: 'HTTP/1.1 400 Bad Request',
@@ -89,7 +91,8 @@ describe("jQuery Comments Plugin", function () {
             responseText: ''
         });
 
-        comments.jQComments(opts);
+        comments.jQComments({url: getURL});
+
         expect($('div.comment').size()).toBe(0);
         expect(comments).toContainElement('div.error');
         expect(comments).toContainText('Failed to fetch comments from URL...');
